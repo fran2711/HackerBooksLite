6ee5f01a6bfbdb2d1fad42d879e5a9ca76418be7
Fran Lucena <Fran@Fran-MacBook-Pro.local>
2017-02-04 11:12:42 +0100

    started Library

---
+++ HackerBooks/Library.swift
@@ -0,0 +1,26 @@
+//
+//  Library.swift
+//  HackerBooks
+//
+//  Created by Fran Lucena on 4/2/17.
+//  Copyright © 2017 Fran Lucena. All rights reserved.
+//
+
+import Foundation
+
+
+typealias Books = MultiDictionary<Tag, Book>
+typealias TagName = Tag
+
+class Library {
+    
+    // MARK: - Properties
+    
+    var books: Books
+    
+    var tags: [Tag] {
+        
+        
+    }
+    
+}
